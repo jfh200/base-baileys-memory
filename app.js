393df779a4887e134de28f79d2c8e641a5b0ef37
jfh200 <63979696+jfh200@users.noreply.github.com>
2023-09-20 13:15:04 -0600

    actualizacion
    Se agregan las especialidades de ginecología y pediatría

--- app.js
+++ app.js
@@ -6,8 +6,16 @@ const MockAdapter = require('@bot-whatsapp/database/mock')
 
 const flowPrincipal = addKeyword(['hola', 'ole', 'alo', 'buenas']).addAnswer(['🙌 Hola bienvenido al chat de la Clínica San Juan bautista', '¿Como te puedo ayudar el día de hoy?'])
 
+const flowappointment = addKeyword(['cita', 'costos de citas', 'precios de citas','costos', 'precios'])
+                        .addAnswer('Para citas y costos, favor de marcar a los numeros 287 875 20 24 y 287 875 4145')
+
+const flowgynecology = addKeyword(['ginecologo','ginecólogo', 'papanicola', 'ginecología']).addAnswer('Para citas y precios, favor de marcar al numero 287 164 1976')
+const flowpediatrics = addKeyword(['pediatría','pediatria', 'pediatra', 'niños']).addAnswer('Para citas y precios, favor de marcar al numero 287 877 8783')
+
 const flowsecundary = addKeyword('gracias').addAnswer('De nada!')
-const flowinterest = addKeyword(['si', 'medicina general', 'papanicolao']).addAnswer('en construccion')
+
+const flowinterest = addKeyword(['si', 'correcto']).addAnswer('¿En que servicio estas interesado?', null, null,[flowgynecology,flowpediatrics])
+
 const flowservices = addKeyword(['servicios', 'que servicios tienen', 'cuales son tus servicios', 'dame los servicios'])
                     .addAnswer('Nuestros servicios son los siguientes:', null, (ctx,{flowDynamic}) => {
 
@@ -19,9 +27,11 @@ const flowservices = addKeyword(['servicios', 'que servicios tienen', 'cuales so
 
                     .addAnswer('¿Te interesa alguno de nuestros servicios?', {delay:1500}, null, [flowinterest])
 
+               
+
 const main = async () => {
     const adapterDB = new MockAdapter()
-    const adapterFlow = createFlow([flowPrincipal, flowsecundary, flowservices])
+    const adapterFlow = createFlow([flowPrincipal, flowsecundary, flowservices, flowappointment, flowgynecology, flowpediatrics])
     const adapterProvider = createProvider(BaileysProvider)
 
     createBot({
